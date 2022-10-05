8ccb77ee424b9da413a50fa6a979c52c933c851d
miche <miche@10.232.11.211>
2022-10-04 18:22:29 -0700

    Proyecto de prueba

--- src/mx/itson/canela/entidades/Ingredientes.java
+++ src/mx/itson/canela/entidades/Ingredientes.java
@@ -6,9 +6,25 @@ package mx.itson.canela.entidades;
 
 /**
  *
- * @author miche
+ * @author michelle
  */
 public class Ingredientes {
     
+
+    private String nombre;
     
-}
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    }
--- src/mx/itson/canela/entidades/Paso.java
+++ src/mx/itson/canela/entidades/Paso.java
@@ -10,4 +10,34 @@ package mx.itson.canela.entidades;
  */
 public class Paso {
     
+     private int orden;
+    private String descripcion;
+    
+    /**
+     * @return the orden
+     */
+    public int getOrden() {
+        return orden;
+    }
+
+    /**
+     * @param orden the orden to set
+     */
+    public void setOrden(int orden) {
+        this.orden = orden;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
